610926420dddcd33acb9d70dca527ade6597e463
dlm589 <dlm589@yahoo.com.tw>
2023-05-31 17:33:24 -0400

    Michael edited the page
    1. changed background colour to dark blue (just trying things out)
    2. added a heading "brief history"
    3. added text (copied from wikepedia, will remove later)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
 
     import "../assets/global-styles.css"
+    let name = "Michael"
+
 
 </script>
 
@@ -11,6 +13,8 @@
     <h1>Bike Share Toronto</h1>
 
     <p>Charts and text on the growth of bike share ridership in Toronto</p>
+    <h1> A Brief History </h1>
+    <p> {name} edited: Bike Share Toronto is a bicycle-sharing system in Toronto, Canada. The system consists of 6850 bicycles and 625 stations, and covers over 200 square kilometres (80 square miles) of the city, from Finch Avenue in the north, Rouge Park in the east, Lake Ontario to the south, and to Long Branch to the west.[1][2] Bike Share Toronto recorded 3,575,000 trips in 2021.[3] The system was launched in 2011 by PBSC Urban Solutions under the BIXI brand and was taken over by the Toronto Parking Authority in 2014. (Copied from Wikepedia, only for testing)</p>
     
 </main>
 
@@ -21,7 +25,7 @@
     :global(body) {
 		padding: 0px;
 		margin: 0px;
-		background-color: var(--brandDarkGreen);
+		background-color: var(--brandDarkBlue);
 	}
 
 	main {
@@ -41,6 +45,9 @@
         /* should pick a consistent font here probably */
         font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
         color: var(--brandWhite);
+        text-align: left;
+        padding-left: 10%;
+        padding-right: 10%;
     }
 
 </style>
