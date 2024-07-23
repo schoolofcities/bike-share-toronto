ad0855c02dcc1a0f59e6f5f9d2f194722dd61a98
Jeff Allen <jeffallen89@gmail.com>
2024-07-23 14:22:59 -0400

    re organize

--- data-preparation/trips/polylines-to-geopandas.py
+++ data-preparation/trips/polylines-to-geopandas.py
@@ -8,7 +8,7 @@ def decode_polyline(encoded_polyline):
 
 	try:
 		coordinates = polyline.decode(encoded_polyline)
-		coordinates = [(lon + 0.0002 * random.random() ** 3, lat + 0.0002 * random.random() ** 3) for lat, lon in coordinates]
+		coordinates = [(lon + 0.0002 * random.random() ** 3 - + 0.0002 * random.random() ** 3, lat + 0.0002 * random.random() ** 3 - + 0.0002 * random.random() ** 3) for lat, lon in coordinates]
 		return LineString(coordinates)
 	except:
 		return None
@@ -22,5 +22,5 @@ df = gpd.GeoDataFrame(df, geometry='geometry')
 
 df.crs = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
 
-df.to_file("trips/trips-test-2.shp")
+df.to_file("trips/trips-all.shp")
 
---
+++ src/routes/trips-june2024/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+    import TopSofC from "../../lib/TopSofC.svelte";
+    
+</script>
+
+<TopSofC />
+
+
+
+<main>
+    <div class="title">
+        <h1>One Month of Bike Share Trips in Toronto</h1>
+        <p>
+            <a href="http://jamaps.github.io">Jeff Allen</a> -- 08/2024
+        </p>
+    </div>
+
+    <div class="text">
+        <p>
+            Hello world
+        </p>
+    </div>
+</main>
+
+<style>
+
+</style>
