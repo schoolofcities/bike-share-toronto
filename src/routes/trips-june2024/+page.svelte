40be8ff6cd260f57ace436800cee89a5281b509e
Jeff Allen <jeffallen89@gmail.com>
2024-07-29 10:51:14 -0400

    text edit, single-trip.png context

--- src/routes/trips-june2024/+page.svelte
+++ src/routes/trips-june2024/+page.svelte
@@ -16,7 +16,7 @@
 	<main>
 		<div class="title">
 			<h1>Toronto's Bike Share Geography</h1>
-			<p>Estimating and mapping routes for every Bike Share trip in June 2024 based on origin-destination ridership data and a cycling network graph. Click on the image to view at a higher resolution.</p>
+			<p>Estimating and mapping routes for almost every Bike Share trip in June 2024 based on origin-destination ridership data and a cycling network graph. 762,760 trips in total! Click on the image to view at a higher resolution.</p>
 			<p>
 				<a href="http://jamaps.github.io" target="_blank">Jeff Allen</a> / <a href="https://www.schoolofcities.utoronto.ca/" target="_blank">School of Cities</a>
 			</p>
@@ -31,27 +31,32 @@
 		<div class="text">
 			<h2>Why & How</h2>
 			<p>
-				We've seen a lot of <a href="./growth">growth</a> in Bike Share ridership in Toronto over the past several years.
-				We recently wanted to A) improve our understanding where people use Bike Share across the city and B) estimate the characteristics of these trips (network distance, speed, elevation gain/loss, etc.) to use in future analyses on how people cycle in the city.
+				There has been substantial <a href="./growth">growth</a> in Bike Share ridership in Toronto over the past several years. We recently wanted to A) improve our understanding about where people use Bike Share and B) estimate the characteristics of these trips (network distance, speed, elevation gain/loss, etc.) to use in future analyses on how people cycle in the city.
 			</p>
 			<p>
-				From Bike Share Toronto, we graciously received a dataset which included a record for every trip in June 2024 including when and where trips started and ended as well as attributes like the type of bike (electric or classic) and type of user (annual member or pay-per-ride). But we didn't know the route taken, the distance of the trip, and the elevation gain or loss. 
+				From Bike Share Toronto, we graciously received a dataset which included a record for every trip in June 2024. There were 764,177 records in this dataset. 762,760 of these represented normal trips that were not terminated shortly after a bike was unlocked and returned to the same station. That's over 25,000 trips per day!
+			</p>
+			<p>
+				Each record in this dataset had information about when and where trips started and ended as well as attributes like the type of bike (electric or classic) and type of user (annual member or pay-per-ride). But we didn't know the route taken, the distance of the trip along this route, and the elevation gain or loss. 
 				<!-- We can pretty easily calculate the straight line (i.e. as the crow flies) distance between the start and end stop, but in reality, the distance cyclists travel is longer, since there is rarely a perfectly straight path between the start and end locations. So -->
-				We set out to estimate this route information with the help of open data and software, and then once calculated, layer the route for every trip on a map to look at overall travel patterns.
+				We set out to estimate this route information with the help of open data and software. Once this was calculated, we layered the route for every trip onto a map to look at overall travel patterns.
 			</p>
 			<p>
-				To do this, we needed the help of a <a href="https://en.wikipedia.org/wiki/Graph_(discrete_mathematics)" target="_blank">graph</a> representing the cycling network in the City of Toronto. We created this using network data from <a href="https://www.openstreetmap.org/#map=14/43.6586/-79.3833" target="_blank">OpenStreetMap</a> and the open source routing software <a href="https://www.openstreetmap.org/#map=14/43.6586/-79.3833" target="_blank">GraphHopper</a> to build a routable network graph that works off a local computer. Each 'edge' in the network is weighted based the type of path (e.g. designated cycling trails away from car, like the Waterfront Trail, are up-weighted; busy car-centric roads without cycling infrastructure are down-weighted). From this network graph we can make queries, like to Google Maps and similar routing applications, where we input the starting location, end location, and ask the graph to return us the most plausible route, along with information on trip duration, distance, and change in elevation. The upshot is that since we have this data and tools stored locally, is that we can do calculations it at scale, i.e. calculate 1000s of routes in just a few seconds.
+				To do this, we needed the help of a <a href="https://en.wikipedia.org/wiki/Graph_(discrete_mathematics)" target="_blank">graph</a> representing the cycling network in the City of Toronto. We created this using network data from <a href="https://www.openstreetmap.org/#map=14/43.6586/-79.3833" target="_blank">OpenStreetMap</a> and <a href="https://www.openstreetmap.org/#map=14/43.6586/-79.3833" target="_blank">GraphHopper</a>, an open source network analysis software, to build a routable network graph that works off a local computer. Each line segment in the network is weighted by its length but also its attributes (e.g. designated cycling trails away from car, like the Waterfront Trail, are up-weighted; busy car-centric roads without cycling infrastructure are down-weighted). From this network graph we can make queries, similar to Google Maps, where we input the start location and end location, and ask the graph to return us the most plausible route along with information on trip duration, distance, and change in elevation. Here's an example for a short trip.
+			</p>
+			<img src="/bike-share-toronto/single-trip.png">	
+			<p>	
+				The upshot is that since we have this data and software stored locally, is that we can do calculations it at scale, i.e. calculate 1,000s of routes like this in just a few seconds.
 			</p>
 			<p>
-				One tricky consideration is what to do for trips that begin and end at the same docking station. If we input the same start and end location into the network graph, it will return a trip of length = 0. But in reality, these are trips that people will likely ride somewhere and then cycle back, e.g. for leisure rather than commuting. Since we don't know where they actually travel to, we used a random number generator to estimate a location within a 5km radius of the starting Bike Share station. This number is a bit arbitrary, but was based on our best guess on how far most people would bike from/to a single location for leisure. We made a couple exceptions to this for the stations with the most of these return trips, which were along the waterfront and the base of the Leslie Spit. For these we weighted this random number location generator to locations on the Leslie Spit, the Waterfront, and Toronto Island depending on the location of the docking station. 
+				One tricky consideration is what to do for trips that begin and end at the same docking station. If we input the same start and end location into the network graph, it will return a trip of length = 0. But in reality, these are trips that people will likely ride somewhere and then cycle back to where they started, often for leisure rather than commuting. Since we don't know where they actually travel to, we used a random number generator to estimate a location within a 5km radius of the starting Bike Share station. This of course is a bit arbitrary, but was based on our best guess on how far most people would bike from/to a single location for leisure. We made a couple exceptions to this for the stations with the most of these out-and-back leisure trips, which were along the waterfront and the base of the Leslie Spit. For these we constrained this random number generator to locations on the Leslie Spit, the Waterfront, and Toronto Island depending on the location of the docking station. 
 			</p>
 			<p>
-				We used this output to create the visualization at the top of this page, by overlaying all ~764k routes onto a single map. There were a couple of visual tricks here to help the visualization. We add some noise to the path geometry (i.e. randomly jitter its coordinates a bit) and then visualized each path as a very very very thin line. The goal here was to help highlight segments of greater travel flow, both via brightness and line-width.
-				Here's a super zoomed in view to a bundle of trips on the Danforth near Pape.
+				The output of this data crunching allowed us to create the visualization at the top of this page, by overlaying all 762,760 routes onto a single map. There were a couple of visual tricks here to help the visualization. We add some noise to the path geometry (i.e. randomly jitter its coordinates a bit) and then visualized each path as a very very very thin line. The goal here was to help highlight segments of greater travel flow, both via brightness and line-width. Here's a super zoomed in view to a bundle of trips on the Danforth near Pape.
 			</p>
 			<img src="/bike-share-toronto/many-trips.png">
 			<p>
-				This map at the top of this page was tinkered with in GIMP using a combination of colour and filter settings. The 'bloom' filter was used to try to highlight more well-travelled paths. GIMP is a bit of a rabbit hole; one can tinker with colours and effects for hours. Below are a couple interesting iterations, first is pixelate and cartoon filter to create a Tanaka-esque heat map, the second using an oilify filter and some curvature blur to create a water colour effect.
+				The map at the top of this page was tinkered with in <a href="https://www.gimp.org/" target="_blank">GIMP</a> using a combination of colour and filter settings. The 'bloom' filter was used to try to highlight more well-traveled paths. GIMP is a bit of a rabbit hole; one can tinker with colours and effects for hours. Below are a couple interesting iterations, first is pixelate and cartoon filter to create a gridded heat map, the second using curvature blur to create a water colour effect.
 			</p>
 			<p>
 				Anyways, that's it for now, thanks for viewing / reading!
